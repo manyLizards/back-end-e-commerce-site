ec7396bfaae1bc07798bbee7f2cfe63b889f29ee
Jordan <jnbarringer96@gmail.com>
2022-12-09 14:31:07 -0600

    added categories endpoint

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+router.get('/', (req, res) => {
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  })
+})
 
 router.get('/', (req, res) => {
   // find all categories
