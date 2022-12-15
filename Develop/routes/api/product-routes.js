c48a162da5c7d8ed8aa6df2f9630d59814985488
Jordan <jnbarringer96@gmail.com>
2022-12-15 13:34:47 -0600

    added the ability to create a new product

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,17 +6,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll()
   // be sure to include its associated Category and Tag data
+  .then((productData) => {
+    res.json(productData);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  Product.findByPk(req.params.id).then((productData) => {
+    res.json(productData)
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create(req.body)
+  .then((newProduct) => {
+    res.json(newProduct);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
   /* req.body should look like this...
     {
       product_name: "Basketball",
