f561f4aae3e739f88cfbf77adb70edeab9814046
Jordan <jnbarringer96@gmail.com>
2022-12-15 13:47:51 -0600

    added api endpoints

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
+router.use('/tags', tagRoutes);
 
 router.get('/', (req, res) => {
   // find all tags
