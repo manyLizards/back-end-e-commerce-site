c9c6da7c1156967609ca4e58589e0dd513a8ae2d
Jordan <jnbarringer96@gmail.com>
2022-12-15 18:02:32 -0600

    fixed endpoint

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-router.use('/categories', categoryRoutes);
+
 
 router.get('/', (req, res) => {
   // find all categories
